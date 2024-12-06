59802aa846e5bd2fcbb3460a185be0563f1e452a
Dicle Bölek <132748792+diclebolek@users.noreply.github.com>
2024-12-06 17:34:43 +0300

    Add files via upload

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -13,61 +13,107 @@ namespace WEB3.Data
         }
 
         // Tablolarınızı temsil eden DbSet özelliklerini tanımlayın:
-        public DbSet<Salon> Salons { get; set; }
-        public DbSet<Services> Services { get; set; }
-        public DbSet<Employee> Employees { get; set; }
-        public DbSet<EmployeeAvailability> EmployeeAvailabilities { get; set; }
-        public DbSet<Appointments> Appointments { get; set; }
-        public DbSet<AppointmentStatus> AppointmentStatuses { get; set; } // Düzeltilmiş: AppointmentStatuses
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<CustomerAppointment> CustomerAppointments { get; set; }
-        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Services> services { get; set; }
+        public DbSet<Employees> employees { get; set; }
+        public DbSet<EmployeeAvailability> employeeavailability { get; set; }
+        public DbSet<Appointments> appointments { get; set; }
+        public DbSet<AppointmentStatus> appointmentstatus { get; set; } // Düzeltilmiş: AppointmentStatuses
+        public DbSet<Customer> customer { get; set; }
+        public DbSet<CustomerAppointment> customerappointments { get; set; }
+        public DbSet<Admin> admin { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Customer>().ToTable("customer");
 
-            // AppointmentStatus ve Appointment ilişkisini tanımlıyoruz
-            modelBuilder.Entity<AppointmentStatus>()
-                .HasKey(a => a.approvalStatus);  // approvalStatus'u birincil anahtar olarak belirtiyoruz.
+            // Admin tablosu
+            modelBuilder.Entity<Admin>().ToTable("admin");
 
-            modelBuilder.Entity<AppointmentStatus>()
-                .HasOne(a => a.Appointments)  // appointmentId ile Appointment arasında ilişki
-                .WithMany()  // Appointment, birçok AppointmentStatus'e sahip olabilir.
-                .HasForeignKey(a => a.appointmentId)  // appointmentId'nin yabancı anahtar olduğunu belirtiyoruz.
-                .OnDelete(DeleteBehavior.Cascade);  // Appointment silindiğinde ilgili AppointmentStatus da silinsin.
+            // Employee tablosu
+            modelBuilder.Entity<Employees>().ToTable("employees");
 
-            modelBuilder.Entity<AppointmentStatus>()
-                .HasOne(a => a.Admins)  // adminId ile Admin arasında ilişki
-                .WithMany()  // Admin, birçok AppointmentStatus'e sahip olabilir.
-                .HasForeignKey(a => a.adminId)  // adminId'nin yabancı anahtar olduğunu belirtiyoruz.
-                .OnDelete(DeleteBehavior.Cascade);  // Admin silindiğinde ilgili AppointmentStatus da silinsin.
+            // EmployeeAvailability tablosu
+            modelBuilder.Entity<EmployeeAvailability>().ToTable("employeeavailability");
 
-            // Employee ile ilişki
-            modelBuilder.Entity<Appointments>()
-                .HasOne(a => a.Employee) // Employee ile ilişki
-                .WithMany()  // Employee, birçok Appointment'a sahip olabilir
-                .HasForeignKey(a => a.employeeId);  // employeeId yabancı anahtar olarak kullanılır
+            // Appointment tablosu
+            modelBuilder.Entity<Appointments>().ToTable("appointments");
 
-            // Service ile ilişki
-            modelBuilder.Entity<Appointments>()
-                .HasOne(a => a.Service) // Service ile ilişki
-                .WithMany()  // Service, birçok Appointment'a sahip olabilir
-                .HasForeignKey(a => a.serviceId);  // serviceId yabancı anahtar olarak kullanılır
+            // AppointmentStatus tablosu
+            modelBuilder.Entity<AppointmentStatus>().ToTable("appointmentstatus");
 
-            // Customer ile ilişki
-            modelBuilder.Entity<Appointments>()
-                .HasOne(a => a.Customer) // customerId ile Customer arasında ilişki
-                .WithMany()  // Customer, birçok Appointment'a sahip olabilir
-                .HasForeignKey(a => a.customerId)  // customerId yabancı anahtar olarak kullanılır
-                .OnDelete(DeleteBehavior.Cascade);  // Customer silindiğinde ilgili Appointment da silinsin
+            // Service tablosu
+            modelBuilder.Entity<Services>().ToTable("services");
 
-            // EmployeeAvailability ile Employee arasındaki ilişkiyi tanımlıyoruz
+            // CustomerAppointments tablosu
+            modelBuilder.Entity<CustomerAppointment>().ToTable("customerappointments");
+
+            //***************************************
             modelBuilder.Entity<EmployeeAvailability>()
-                .HasOne(ea => ea.Employee) // EmployeeAvailability ile Employee arasındaki ilişki
-                .WithMany()  // Employee, birçok EmployeeAvailability'ye sahip olabilir
-                .HasForeignKey(ea => ea.employeeId)  // employeeId yabancı anahtar olarak kullanılır
-                .OnDelete(DeleteBehavior.Cascade);  // Employee silindiğinde ilgili EmployeeAvailability'ler de silinsin
+                .HasOne(ea => ea.employeeids)
+                .WithMany()
+                .HasForeignKey(ea => ea.employeeid)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Employee ve EmployeeAvailability arasındaki ilişkiyi tanımlıyoruz
+            modelBuilder.Entity<Employees>()
+                .HasOne(e => e.employeeavailability)
+                .WithOne()
+                .HasForeignKey<Employees>(e => e.availabilityid)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Appointments ve Employees arasındaki ilişki
+            modelBuilder.Entity<Appointments>()
+                .HasOne(a => a.employeeids)  // Appointments ile Employee arasında ilişki
+                .WithMany()  // Employee, birçok Appointment'a sahip olabilir
+                .HasForeignKey(a => a.employeeid)  // employeeId'nin yabancı anahtar olduğunu belirtiyoruz
+                .OnDelete(DeleteBehavior.Cascade);  // Employee silindiğinde ilgili Appointment'lar da silinsin
+
+            // Appointments ve Customers arasındaki ilişki
+            modelBuilder.Entity<Appointments>()
+                .HasOne(a => a.customerids)  // Appointments ile Customer arasında ilişki
+                .WithMany()  // Customer, birçok Appointment'a sahip olabilir
+                .HasForeignKey(a => a.customerid)  // customerId'nin yabancı anahtar olduğunu belirtiyoruz
+                .OnDelete(DeleteBehavior.Cascade);  // Customer silindiğinde ilgili Appointment'lar da silinsin
+
+            // Appointments ve AppointmentStatus arasındaki ilişki
+            modelBuilder.Entity<Appointments>()
+                .HasOne(a => a.approvalstatuses)  // Appointments ile AppointmentStatus arasında ilişki
+                .WithMany()  // AppointmentStatus, birçok Appointment'a sahip olabilir
+                .HasForeignKey(a => a.approvalstatus)  // appointmentStatusId'nin yabancı anahtar olduğunu belirtiyoruz
+                .OnDelete(DeleteBehavior.Cascade);
+            // CustomerAppointments ve Customer arasındaki ilişkiyi tanımlıyoruz
+            modelBuilder.Entity<CustomerAppointment>()
+                .HasOne(ca => ca.customer)  // CustomerAppointments ile Customer arasında ilişki
+                .WithMany()  // Customer, birçok CustomerAppointments'e sahip olabilir
+                .HasForeignKey(ca => ca.customerid)  // customerId'nin yabancı anahtar olduğunu belirtiyoruz
+                .OnDelete(DeleteBehavior.Cascade);  // Customer silindiğinde ilgili CustomerAppointments'ler de silinsin
+
+            // CustomerAppointments ve Services arasındaki ilişkiyi tanımlıyoruz
+            modelBuilder.Entity<CustomerAppointment>()
+                .HasOne(ca => ca.services)  // CustomerAppointments ile Services arasında ilişki
+                .WithMany()  // Service, birçok CustomerAppointments'e sahip olabilir
+                .HasForeignKey(ca => ca.serviceid)  // serviceId'nin yabancı anahtar olduğunu belirtiyoruz
+                .OnDelete(DeleteBehavior.Cascade);  // Service silindiğinde ilgili CustomerAppointments'ler de silinsin
+
+            // CustomerAppointments ve Employees arasındaki ilişkiyi tanımlıyoruz
+            modelBuilder.Entity<CustomerAppointment>()
+                .HasOne(ca => ca.employees)  // CustomerAppointments ile Employee arasında ilişki
+                .WithMany()  // Employee, birçok CustomerAppointments'e sahip olabilir
+                .HasForeignKey(ca => ca.employeeid)  // employeeId'nin yabancı anahtar olduğunu belirtiyoruz
+                .OnDelete(DeleteBehavior.Cascade);  // Employee silindiğinde ilgili CustomerAppointments'ler de silinsin
+
+            // CustomerAppointments ve Appointments arasındaki ilişkiyi tanımlıyoruz
+            modelBuilder.Entity<CustomerAppointment>()
+                .HasOne(ca => ca.appointments)  // CustomerAppointments ile Appointment arasında ilişki
+                .WithMany()  // Appointment, birçok CustomerAppointments'e sahip olabilir
+                .HasForeignKey(ca => ca.appointmentid)  // appointmentId'nin yabancı anahtar olduğunu belirtiyoruz
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+
+
+
         }
     }
 }
